07fd8aa5eac9935f05c183b5a690e52bf180f0d3
mattibun <mattibun@gmail.com>
2019-09-07 18:38:43 -0400

    Basic reset complete

--- etch.js
+++ etch.js
@@ -1,30 +1,47 @@
-function setBoard() {
+function setBoard(side) {    
     let body = document.querySelector('body');
     // Create outer container
     let etch = document.createElement('div');
-    etch.setAttribute('id', "etch");    
+    etch.setAttribute('id', "etch");
     // Generate grid    
-    body.appendChild(generateGrid(etch));
+    body.appendChild(generateGrid(etch,side));    
+    // Configure button
+    button = document.querySelector('button');
+    button.addEventListener('click', resetBoard);
 }
 
-function generateGrid(board, num=16) {
+function generateGrid(board, num = 16) {
     for (i = 0; i < num; i++) {
         let etchBlock = document.createElement('div');
         etchBlock.classList.add('etchBlock', i);
         board.appendChild(etchBlock);
-    }
+    }    
     return board;
 }
 
-
-function etch() {
+function setSquareEvents() {
+    console.log('Enter etch');
     const squares = document.getElementsByClassName("etchBlock");
     Array.from(squares).forEach((square) => {
         square.addEventListener('mouseover', (e) => {
-            square.classList.add('colored');            
+            square.classList.add('colored');
         });
     })
 }
 
+function resetBoard() {
+    /*
+    const squares = document.getElementsByClassName("etchBlock");
+    Array.from(squares).forEach((square) =>
+        square.classList.remove('colored')
+    )*/
+    side = prompt("How many squares should be on a side?");
+    let etch = document.getElementById('etch');
+    let body = document.querySelector('body');
+    body.removeChild(etch);
+    setBoard(side);
+    setSquareEvents();
+}
+
 setBoard();
-etch();
+setSquareEvents();

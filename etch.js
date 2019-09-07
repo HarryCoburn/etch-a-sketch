dc8921c69ebf719a71babd3cabaa9d5932e5d680
mattibun <mattibun@gmail.com>
2019-09-07 18:13:36 -0400

    squares transform properly on mouseover

--- etch.js
+++ etch.js
@@ -3,12 +3,23 @@ function setBoard() {
     // Create outer container
     let etch = document.createElement('div');
     etch.setAttribute('id', "etch");    
+    // Generate grid
     for (i = 0; i < 16; i++) {
         let etchBlock = document.createElement('div');
-        etchBlock.classList.add('etchBlock' + i);
+        etchBlock.classList.add('etchBlock', i);
         etch.appendChild(etchBlock);
     }
     body.appendChild(etch);
 }
 
+function etch() {
+    const squares = document.getElementsByClassName("etchBlock");
+    Array.from(squares).forEach((square) => {
+        square.addEventListener('mouseover', (e) => {
+            square.classList.add('colored');            
+        });
+    })
+}
+
 setBoard();
+etch();

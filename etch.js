a3c4ebf41b9f652b746afc8125a10b46c159d395
mattibun <mattibun@gmail.com>
2019-09-07 20:58:57 -0400

    Added two more modes

--- etch.js
+++ etch.js
@@ -1,17 +1,32 @@
-function setBoard(side = 4) {    
-    if (side === "") side = 4;
+function setBoard(side = 4, boardType = "black") {    
+    if (side === "" || side === null) side = 4;
     let body = document.querySelector('body');
     // Create outer container
     let etch = document.createElement('div');
     etch.setAttribute('id', "etch");
-    etch.setAttribute('style', 'grid-template-rows: repeat(' + side + ', 1fr); grid-template-columns: repeat(' + side + ', 1fr);')    
+    etch.setAttribute('style', 'grid-template-rows: repeat(' + side + ', 1fr); grid-template-columns: repeat(' + side + ', 1fr);')
+    if (boardType === "greyscale") {
+        etch.style.backgroundColor = "white"
+    } else {
+        etch.style.backgroundColor = "gray";
+    }
     // Generate grid    
     body.appendChild(generateGrid(etch,side*side));    
-    // Configure button
-    button = document.querySelector('button');
-    button.addEventListener('click', resetBoard);
+    // Configure buttons
+    blackButton = document.querySelector('button[id="black"]');
+    blackButton.addEventListener('click', resetBoard, false);
+    blackButton.boardType = "black";
+
+    rainbowButton = document.querySelector('button[id="rainbow"]');
+    rainbowButton.addEventListener('click', resetBoard, false);
+    rainbowButton.boardType = "rainbow";
+
+    greyButton = document.querySelector('button[id="greyscale"]');
+    greyButton.addEventListener('click', resetBoard, false);
+    greyButton.boardType = "greyscale";
+
     // Set square events
-    setSquareEvents();
+    setSquareEvents(boardType);
 }
 
 function generateGrid(board, num) {
@@ -23,22 +38,63 @@ function generateGrid(board, num) {
     return board;
 }
 
-function setSquareEvents() {
-    console.log('Enter etch');
+function setSquareEvents(boardType) {    
     const squares = document.getElementsByClassName("etchBlock");
-    Array.from(squares).forEach((square) => {
-        square.addEventListener('mouseover', (e) => {
-            square.classList.add('colored');
-        });
-    })
+    if (boardType === "black") {
+        Array.from(squares).forEach((square) => {
+            square.addEventListener('mouseover', (e) => {
+                square.setAttribute('style', 'background-color: rgb(0,0,0)');
+                square.classList.add('colored');
+            });
+        })
+    } else if (boardType === "rainbow") {
+        Array.from(squares).forEach((square) => {
+            square.addEventListener('mouseover', (e) => {
+                if (!square.classList.contains('colored')) {
+                    square.setAttribute('style', 'background-color: rgb(' + getRandomColor() + ', ' + getRandomColor() + ', ' + getRandomColor() +')');
+                    square.classList.add('colored');
+                } else {
+                    shadeColor(square);
+                }  
+                
+            });
+        })
+    } else if (boardType === "greyscale") {
+        Array.from(squares).forEach((square) => {
+            square.addEventListener('mouseover', (e) => {
+                if (!square.classList.contains('colored')) {
+                    square.setAttribute('style', 'background-color: rgb(230,230,230)');
+                    square.classList.add('colored');
+                } else {
+                shadeColor(square);
+                }
+            });
+        })
+    }
 }
 
-function resetBoard() {
+function getRandomColor() {
+    return Math.round(Math.random()*255);
+}
+
+function shadeColor(square) {
+    let style = getComputedStyle(square);
+    let rgb = style.backgroundColor.match(/\d+/g);   
+    let newRGB = [];
+    rgb.forEach((color) => {
+        color = parseInt(color * 90 / 100);
+        color = (color>0)?color:0;
+        newRGB.push(color);
+    })
+    square.setAttribute('style', 'background-color: rgb(' + newRGB[0] + ', ' + newRGB[1] + ', ' + newRGB[2] +')');                    
+}
+
+function resetBoard(evt) {
     side = prompt("How many squares should be on a side?");
     let etch = document.getElementById('etch');
     let body = document.querySelector('body');
     body.removeChild(etch);
-    setBoard(side);    
+    setBoard(side, evt.target.boardType);    
 }
 
 setBoard();

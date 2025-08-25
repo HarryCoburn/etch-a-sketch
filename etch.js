598113680eb3413bf6b4dd9b19508954d8acca46
HarryCoburn <10048077+HarryCoburn@users.noreply.github.com>
2025-08-25 08:23:12 -0400

    Basic Etch-a-sketch complete
    The basic code is in place. Next steps are to style it better and
    make the extra credit items.

--- etch.js
+++ etch.js
@@ -1,19 +1,41 @@
 let etchContainer = document.querySelector("#etch-container");
 etchContainer.addEventListener("mouseover", colorChange);
 
+
+let gridSizeButton = document.querySelector("#setSize");
+gridSizeButton.addEventListener("click", setGridSize);
+
+
 let gridSize = 4; // Per side
-let numSquares = gridSize * gridSize;
 let containerWidth = 960; // In pixels
 
-// Make the grid
-for (let i = 0; i < numSquares; i++) {
-    let etchSquare = document.createElement("div");
-    etchSquare.classList.add("square")
-    etchSquare.setAttribute("style", `flex-basis: ${containerWidth/gridSize}px;`)            
-    etchContainer.appendChild(etchSquare)
+function drawGrid() {
+    console.log(gridSize)
+    etchContainer.replaceChildren()
+    let numSquares = gridSize * gridSize;
+    for (let i = 0; i < numSquares; i++) {
+        console.log(`Making square number ${i}`)
+        let etchSquare = document.createElement("div");
+        etchSquare.classList.add("square")
+        etchSquare.setAttribute("style", `flex-basis: ${containerWidth / gridSize}px;`)
+        etchContainer.appendChild(etchSquare)
+    }
 }
 
-function colorChange(event) {    
-    let target = document.elementFromPoint(event.clientX, event.clientY);   
+// Handle the colors of the grid
+function colorChange(event) {
+    let target = document.elementFromPoint(event.clientX, event.clientY);
     target.style.backgroundColor = "green";
 }
+
+
+function setGridSize(event) {
+    let newSize = Number(prompt("How many squares per side should the grid be? (1-100)"))
+    while ((newSize < 0) || (newSize > 100) || (!Number.isInteger(newSize))) {
+        newSize = Number(prompt("How many squares per side should the grid be? (1-100)"))
+    }
+    gridSize = newSize
+    drawGrid()
+}
+
+drawGrid()

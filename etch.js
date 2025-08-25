2e3ec96be191325c1c84998c6dcfb7635b332614
HarryCoburn <10048077+HarryCoburn@users.noreply.github.com>
2025-08-24 22:49:00 -0400

    Etch-a-sketch basics working.
    Grid now works with the mouseover events properly. Single
    event handler as well.

--- etch.js
+++ etch.js
@@ -1,9 +1,19 @@
 let etchContainer = document.querySelector("#etch-container");
+etchContainer.addEventListener("mouseover", colorChange);
 
-let gridSize = 16;
+let gridSize = 4; // Per side
+let numSquares = gridSize * gridSize;
+let containerWidth = 960; // In pixels
 
-for (let i = 0; i < gridSize; i++) {
+// Make the grid
+for (let i = 0; i < numSquares; i++) {
     let etchSquare = document.createElement("div");
     etchSquare.classList.add("square")
+    etchSquare.setAttribute("style", `flex-basis: ${containerWidth/gridSize}px;`)            
     etchContainer.appendChild(etchSquare)
+}
+
+function colorChange(event) {    
+    let target = document.elementFromPoint(event.clientX, event.clientY);   
+    target.style.backgroundColor = "green";
 }

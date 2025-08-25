a8fc8effcdff50753606760e2cdacd7dcbcf24dc
HarryCoburn <10048077+HarryCoburn@users.noreply.github.com>
2025-08-25 08:52:53 -0400

    Rainbow colors implemented
    Finished the rainbow color challenge. Added a toggle button as well.

--- etch.js
+++ etch.js
@@ -5,9 +5,20 @@ etchContainer.addEventListener("mouseover", colorChange);
 let gridSizeButton = document.querySelector("#setSize");
 gridSizeButton.addEventListener("click", setGridSize);
 
+let rainbowButton = document.querySelector("#rainbowToggle")
+rainbowButton.addEventListener("click", () => {
+    rainbowOn = !rainbowOn;
+    if (rainbowOn) {
+        rainbowButton.textContent = "Rainbow ON";
+    } else {
+        rainbowButton.textContent = "Rainbow OFF";
+    }
+})
+
 
 let gridSize = 4; // Per side
 let containerWidth = 960; // In pixels
+let rainbowOn = false;
 
 function drawGrid() {
     console.log(gridSize)
@@ -24,8 +35,29 @@ function drawGrid() {
 
 // Handle the colors of the grid
 function colorChange(event) {
+    function getRandomColor() {
+        return Math.round(Math.random() * 255);
+    }
+
     let target = document.elementFromPoint(event.clientX, event.clientY);
-    target.style.backgroundColor = "green";
+    if (rainbowOn) {
+        target.style.backgroundColor = `rgb(${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()})`;
+    } else {
+        target.style.backgroundColor = `black`;
+    }
+}
+
+
+function shadeColor(square) {
+    let style = getComputedStyle(square);
+    let rgb = style.backgroundColor.match(/\d+/g);
+    let newRGB = [];
+    rgb.forEach((color) => {
+        color = parseInt(color * 90 / 100);
+        color = (color > 0) ? color : 0;
+        newRGB.push(color);
+    })
+    square.setAttribute('style', 'background-color: rgb(' + newRGB[0] + ', ' + newRGB[1] + ', ' + newRGB[2] + ')');
 }
 
 
